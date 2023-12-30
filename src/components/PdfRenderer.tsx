29f24959aa5c93c1ec59e8752609d66fa87a353c
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-12-30 10:50:10 +0100

    add pdf view page

---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,14 @@
+
+const PdfRenderer = () => {
+  return (
+    <div className="w-full bg-white rounded-md shadown flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">
+          top bar
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PdfRenderer
